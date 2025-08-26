d42ff8aa6ea9cd2c5a0081f909b2d11c97e75a01
YuHong <yuhong119@hellobike.com>
2025-08-26 14:21:00 +0800

    1-3: Adding a Version Number and Configured Header File

--- tutorial.cxx
+++ tutorial.cxx
@@ -2,9 +2,13 @@
 #include <iostream>
 #include <string>
 
+#include "TutorialConfig.h"
+
 int main(int argc, char* argv[]) {
     if (argc < 2) {
-        std::cout << "Usgae: " << argv[0] << " number" << std::endl;
+        std::cout << argv[0] << " Version " << Tutorial_VERSION_MAJOR << "."
+              << Tutorial_VERSION_MINOR << std::endl;
+        std::cout << "Usage: " << argv[0] << " number" << std::endl;
         return 1;
     }
 
